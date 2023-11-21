fe5dbb981f799339611e55e701b1fa29aaa4cb86
jobayerbd327 <jobayerbd327@gmail.com>
2023-11-21 23:36:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//First Answer
+const feedTime = "afternoon";
+if (feedTime === "morning"){
+    console.log("it is morning and time to feed the animals");
+}else if (feedTime === "afternoon"){
+    console.log("it is afternoon and time to feed the animals");
+}else {
+    console.log("now you can't feed the animals.");
+}
+
+//first question answere ends here
+
+
+//Second question Answere starts here
+ const maxWeight =500;
+ const passengerWeight =501;
+ if(maxWeight === passengerWeight ){
+    console.log("passenger can  cross the river safely");
+
+ }else if(maxWeight >= passengerWeight){
+    console.log("passenger can  cross the river safely");
+
+ } else{
+    console.log("passenger can't cross the river safely")
+ }
+
+ //second question answere ends here
+
+
+ // Third Question Answere starts here
+ const myName = "Zobayer";
+ const myHobby = "Playing , Eating Burger, Watching Movie";
+ const sentence =   `hey my Name is ${myName}. My Favourite Hobby Is ${myHobby}.  `;
+ console.log(sentence);
